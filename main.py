b90451bd9a90a95dae402c015eed3f54cbf831b7
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-10-20 18:37:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -5,7 +6,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi.templating import Jinja2Templates
 from dotenv import load_dotenv
 import os
-import asyncio
 
 # Load environment variables
 load_dotenv()
@@ -23,24 +23,21 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Template rendering
 templates = Jinja2Templates(directory="templates")
 
-@app.on_event("startup")
-async def startup_event():
-    # Ensure a fresh event loop is created for each Lambda invocation if necessary
-    try:
-        loop = asyncio.get_event_loop()
-        if loop.is_closed():
-            asyncio.set_event_loop(asyncio.new_event_loop())
-    except RuntimeError:
-        asyncio.set_event_loop(asyncio.new_event_loop())
-
 @app.get("/", response_class=HTMLResponse)
 async def get_home(request: Request):
     # Fetch distinct dates from the collection
     distinct_dates = await collection.distinct("_id")  # Assuming _id is the date
     return templates.TemplateResponse("index.html", {"request": request, "dates": distinct_dates})
 
+
 @app.get("/usage/{date}")
 async def get_usage_data(date: str):
+    # Ensure we are using the correct event loop
+    loop = asyncio.get_event_loop()
+    if loop.is_closed():
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    
     # Find the document for the given date
     document = await collection.find_one({"_id": date})
     if document:
